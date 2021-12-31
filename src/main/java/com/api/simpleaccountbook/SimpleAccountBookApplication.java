314a9150c058c8c582d25c160cbe757e14d3ec6f
daroguzo <kjw2122@naver.com>
2021-12-31 20:39:32 +0900

    Initial commit

---
+++ src/main/java/com/api/simpleaccountbook/SimpleAccountBookApplication.java
@@ -0,0 +1,13 @@
+package com.api.simpleaccountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAccountBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAccountBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/simpleaccountbook/SimpleAccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.simpleaccountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAccountBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
