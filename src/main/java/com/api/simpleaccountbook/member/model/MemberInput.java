ad48143fba5fb3bff48e64ecde03c97e6f0f549c
daroguzo <kjw2122@naver.com>
2022-01-02 15:56:32 +0900

    modify: MemberInput validation 추가

--- src/main/java/com/api/simpleaccountbook/member/model/MemberInput.java
+++ src/main/java/com/api/simpleaccountbook/member/model/MemberInput.java
@@ -1,11 +1,23 @@
 package com.api.simpleaccountbook.member.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter @Builder
 public class MemberInput {
 
+    @Size(min = 4, message = "사용자 이름은 4자 이상 입력해야 합니다.")
+    @NotBlank(message = "사용자 이름은 필수 항목입니다.")
     private String username;
+
+    @Size(min = 4, message = "비밀번호는 4자 이상 입력해야 합니다.")
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
     private String password;
 }
