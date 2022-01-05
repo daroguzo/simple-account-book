c2c826794fa7f7847dbf72f3486f5fd4b5e9812b
daroguzo <kjw2122@naver.com>
2022-01-05 12:07:07 +0900

    modify: 토큰 유효기간 변경

--- src/main/java/com/api/simpleaccountbook/jwt/JwtProperties.java
+++ src/main/java/com/api/simpleaccountbook/jwt/JwtProperties.java
@@ -2,8 +2,8 @@ package com.api.simpleaccountbook.jwt;
 
 public class JwtProperties {
     public static final String SECRET = "jinoo";
-    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 18000; // 3초
-    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 86400000; // 1일
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 60 * 60 * 1000L; // 1시간
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 14 * 24 * 60 * 1000L; // 14일
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
